1e3f81890cd76320fc93adb5aff8b550fc76ad09
Divya <divyajangid9209@gmail.com>
2026-02-13 11:47:19 +0530

    Apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Login from "./pages/Login";
 import Board from "./pages/Board";
 
 function App() {
-  const isLoggedIn = !!localStorage.getItem("user");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route
           path="/"
           element={
-            isLoggedIn ? <Navigate to="/board" replace /> : <Login />
+            isLoggedIn ? <Navigate to="/board" /> : <Login />
           }
         />
         <Route
           path="/board"
           element={
-            isLoggedIn ? <Board /> : <Navigate to="/" replace />
+            isLoggedIn ? <Board /> : <Navigate to="/" />
           }
         />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
